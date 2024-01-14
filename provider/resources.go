d59fd44eaf6f84efc1e04003624f250651a2373d
Ian Wahbe <ian@wahbe.com>
2024-01-13 18:39:57 -0800

    lint_provider.fix

--- provider/resources.go
+++ provider/resources.go
@@ -16,17 +16,20 @@ package dnsimple
 
 import (
 	"fmt"
-	// embed is used to store bridge-metadata.json in the compiled binary
-	_ "embed"
 	"path/filepath"
 	"unicode"
 
-	"github.com/pulumi/pulumi-dnsimple/provider/v3/pkg/version"
+	// embed is used to store bridge-metadata.json in the compiled binary
+	_ "embed"
+
+	"github.com/terraform-providers/terraform-provider-dnsimple/dnsimple"
+
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	tfbridgetokens "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/tokens"
 	shimv2 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v2"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
-	"github.com/terraform-providers/terraform-provider-dnsimple/dnsimple"
+
+	"github.com/pulumi/pulumi-dnsimple/provider/v3/pkg/version"
 )
 
 // all of the token components used below.
